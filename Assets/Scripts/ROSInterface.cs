5e130c9f2f8710b962c2812ac8e26de87643512a
gabrielnaves <gabri.navess@gmail.com>
2017-01-25 00:33:38 -0200

    Implementing the ROS Interface

---
+++ Assets/Scripts/ROSInterface.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ROSInterface : MonoBehaviour {
+
+    public ROSInterface instance { get; private set; }
+    public string receivedMessage { get; private set; }
+
+    private WebSocket socket = new WebSocket(new Uri("ws://localhost:9090"));
+    private bool isConnected;
+
+    void Awake() {
+        if (instance != null)
+            Debug.LogError("[ROSInterface]Awake: Instance already exists");
+        else
+            instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void TestConnection() {
+        StartCoroutine(_TestConnection());
+    }
+
+    private IEnumerator _TestConnection() {
+        yield return StartCoroutine(socket.Connect());
+        if (socket.error != null)
+            Debug.LogError("Error on connection to socket: " + socket.error);
+        else
+            socket.Close();
+    }
+}
