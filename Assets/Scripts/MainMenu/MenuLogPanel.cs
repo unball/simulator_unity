8344c279f90dadb84c74c6169afbfc5a36673bee
gabrielnaves <gabri.navess@gmail.com>
2017-01-24 23:25:38 -0200

    Started the main menu and the websocket interface

---
+++ Assets/Scripts/MainMenu/MenuLogPanel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuLogPanel : MonoBehaviour {
+
+    public MenuLogPanel instance { get; private set; }
+
+    void Awake() {
+        if (instance != null)
+            Debug.LogError("[MenuLogPanel]Awake: Instance already exists");
+        else
+            instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
---
+++ Assets/Scripts/WebSocketInterface.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WebSocketInterface : MonoBehaviour {
+
+    public WebSocketInterface instance { get; private set; }
+
+    void Awake() {
+        if (instance != null)
+            Debug.LogError("[WebSocketInterface]Awake: Instance already exists");
+        else
+            instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
