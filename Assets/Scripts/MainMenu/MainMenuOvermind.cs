b2f297e303c4add87cf44f679f5e8022036e37aa
gabrielnaves <gabri.navess@gmail.com>
2017-01-25 01:21:59 -0200

    Done with the initial menu
    It attempts to coonect to the rosbridge server, and moves on to the next scene if it succeeds. I'll rename this menu on the next commit.

--- Assets/Scripts/MainMenu/MainMenuOvermind.cs
+++ Assets/Scripts/MainMenu/MainMenuOvermind.cs
@@ -9,7 +9,7 @@ public class MainMenuOvermind : MonoBehaviour {
         ct = GetComponent<ConnectionTest>();
         yield return StartCoroutine(ct.TestConnection());
         if (ct.connectionSucceeded)
-            MenuLogPanel.instance.ShowMessage("Connected to rosbridge.");
+            Application.LoadLevel("Simulation");
         else
             MenuLogPanel.instance.ShowMessage(ct.errorMessage +
                 " Make sure rosbridge server is running.");
