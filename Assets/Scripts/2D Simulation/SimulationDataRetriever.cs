a599b2c68612dbb31c77739a55e026b5a693dde1
Gabriel Naves da Silva <gabri.navess@gmail.com>
2017-01-31 16:34:20 -0200

    Started making the simulation management scripts
    For now I'm making a hard-coded simulation parameters script

---
+++ Assets/Scripts/2D Simulation/SimulationDataRetriever.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimulationDataRetriever : MonoBehaviour {
+
+	public static SimulationDataRetriever instance { get; set; }
+
+    public int alliedRobotAmount = 3;
+    public int opponentRobotAmount = 3;
+    public string allySide = "Left";
+
+    public int GetAlliedRobotAmount() {
+        return alliedRobotAmount;
+    }
+
+    public int GetOpponentRobotAmount() {
+        return opponentRobotAmount;
+    }
+
+    public string GetAllySide() {
+        return allySide;
+    }
+
+    void Awake() {
+        if (instance == null)
+            instance = this;
+        else
+            Debug.LogError("[SimulationDataRetriever]Awake: multiple instances of singleton");
+    }
+}
