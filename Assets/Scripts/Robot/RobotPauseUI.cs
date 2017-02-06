4d3ff1e56be1ba5a8b4a99ec404f4cd61bfe84dc
Gabriel Naves da Silva <gabri.navess@gmail.com>
2017-02-06 02:05:12 -0200

    Robots can now be dragged freely when the simulation is paused
    I'll make it possible to rotate them as well.

---
+++ Assets/Scripts/Robot/RobotPauseUI.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class RobotPauseUI : MonoBehaviour {
+
+    public GameObject robotBody;
+
+    private bool isClicked;
+    private Vector2 mouseOffset;
+
+	void Update() {
+        if (Pause.instance.isPaused) {
+            ShowMovementUI();
+            CheckInput();
+        }
+        else
+            HideMovementUI();
+    }
+
+    private void ShowMovementUI() {
+        // Stuff here
+    }
+
+    private void HideMovementUI() {
+        // Stuff here
+    }
+
+    private void CheckInput() {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0)) {
+            isClicked = robotBody.GetComponent<BoxCollider2D>().OverlapPoint(mousePosition);
+            mouseOffset = mousePosition - (Vector2)robotBody.transform.position;
+        }
+        if (Input.GetMouseButtonUp(0))
+            isClicked = false;
+        if (isClicked)
+            robotBody.GetComponent<Rigidbody2D>().MovePosition(mousePosition - mouseOffset);
+    }
+}
