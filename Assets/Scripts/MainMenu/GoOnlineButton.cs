51c1c93d7908838e29644a50fa4a0cd54839e4e7
Gabriel Naves da Silva <gabri.navess@gmail.com>
2017-02-16 19:58:05 -0200

    It's possible to go online after entering offline mode now

---
+++ Assets/Scripts/MainMenu/GoOnlineButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoOnlineButton : MonoBehaviour {
+
+    public void ReturnToConnectionTestScreen() {
+        SceneManager.LoadScene("ConnectionTestScreen");
+    }
+
+    void Start() {
+        if (ConnectionMode.IsOnline())
+            gameObject.SetActive(false);
+    }
+}
