d69bb35a764e91826f8d88fd35b5207c21650951
Gabriel Naves da Silva <gabri.navess@gmail.com>
2017-03-03 13:34:54 -0300

    Main menu now loads the last used robot amount configuration

---
+++ Assets/Scripts/MainMenu/DropdownOptionSave.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DropdownOptionSave : MonoBehaviour {
+
+    public string playerPrefsKey;
+
+    private Dropdown dropdown;
+
+	void Start () {
+        dropdown = GetComponent<Dropdown>();
+        if (PlayerPrefs.HasKey(playerPrefsKey))
+            dropdown.value = PlayerPrefs.GetInt(playerPrefsKey);
+        else
+            dropdown.value = 3;
+	}
+
+    void OnDestroy() {
+        PlayerPrefs.SetInt(playerPrefsKey, dropdown.value);
+    }
+}
