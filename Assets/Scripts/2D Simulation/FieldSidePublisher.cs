adc7de5f957096648b8b7560ee4f89bfcd60aad9
Gabriel Naves da Silva <gabri.navess@gmail.com>
2017-03-06 00:12:54 -0300

    Simulator now allows changing sides.

---
+++ Assets/Scripts/2D Simulation/FieldSidePublisher.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FieldSidePublisher : MonoBehaviour {
+
+    private Dropdown fieldDropdown;
+    private ROSPublisher publisher;
+
+    public void UpdateFieldSide() {
+        /* This conditional prevents the Dropdown component from calling  */
+        if (Time.timeSinceLevelLoad > 0.1f) {
+            JSONObject fieldMessage = new JSONObject(JSONObject.Type.OBJECT);
+            fieldMessage.AddField("data", fieldDropdown.options[fieldDropdown.value].text);
+            publisher.PublishMessage(fieldMessage);
+        }
+    }
+
+    void Start () {
+        fieldDropdown = GetComponent<Dropdown>();
+        publisher = GetComponent<ROSPublisher>();
+        Invoke("UpdateFieldSide", 0.2f);
+    }
+}
