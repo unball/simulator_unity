d7cba861a0f8ca6d463655f121aec602f789df3c
Gabriel Naves da Silva <gabri.navess@gmail.com>
2017-11-06 19:53:14 -0200

    Simulator now publishes virtual ball position on gui mode

---
+++ Assets/Scripts/2D Simulation/VirtualBallPublisher.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirtualBallPublisher : MonoBehaviour {
+
+	private ROSPublisher publisher;
+
+	void Start() {
+		publisher = GetComponent<ROSPublisher>();
+		if (SimulationDataRetriever.instance.simulationMode != SimulationMode.GUI)
+			this.enabled = false;
+		if (SimulationDataRetriever.instance.ballBehaviour != BallGUIBehaviour.VIRTUAL)
+			this.enabled = false;
+	}
+
+	void Update() {
+		Vector2 ball = Ball.instance.transform.position;
+		JSONObject ballPosMsg = new JSONObject(JSONObject.Type.OBJECT);
+		ballPosMsg.AddField("x", ball.x * 0.1f);
+		ballPosMsg.AddField("y", ball.y * 0.1f);
+		ballPosMsg.AddField("z", 0);
+		if (Time.timeSinceLevelLoad > 0.1f) {
+			publisher.PublishMessage(ballPosMsg);
+			if (publisher.error != null)
+				Debug.LogError(publisher.error);
+		}
+	}
+}
